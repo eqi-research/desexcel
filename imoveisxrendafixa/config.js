f3c318290568df11791a4d1247ac26d57c99118e
renatosos <eqi-research@euqueroinvestir.com>
2026-06-26 16:11:22 -0300

    Configura URL do logger

--- imoveisxrendafixa/config.js
+++ imoveisxrendafixa/config.js
@@ -7,6 +7,6 @@
 // a calculadora funciona normalmente, mas os cálculos não são registrados.
 // ==========================================================================
 window.DESEXCEL_CONFIG = {
-  LOGGER_URL: 'COLE_AQUI_A_URL_DO_APPS_SCRIPT',
+  LOGGER_URL: 'https://script.google.com/macros/s/AKfycbwThPJ6_sH7kCYQuteMaAHmmm7EtTrn5p-nGBw-k_XEp2rsrNW5M5crDYnu3c95-LY7/exec',
   TOOL_ID:    'imoveisxrendafixa'
 };
